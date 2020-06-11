728b776ff8fa3e837c39434a3411e8b76ddf78bd
lavanyagadde <lavanyagadde217@gmail.com>
2020-06-11 12:42:16 -0500

    c

--- ICP2/sourcecode/wordcount.py
+++ ICP2/sourcecode/wordcount.py
@@ -1,7 +1,7 @@
 # Word count
 filename = open('sampletext.txt', 'r')
 line = filename.readline()
-filename_output = open('output.txt', 'w')
+
 wordcount = {}
 while line != '':
     # converting the string into lower case to remove case sensitivity and splitting the string with space
@@ -14,10 +14,15 @@ while line != '':
             wordcount[i] = 1
 
     line = filename.readline()
-for i in wordcount:
-    output = i + ':' + str(wordcount[i])
-    print(output)
-    filename_output.write(output+'\n')
-filename_output.close()
 
+# filename_output = open('output.txt', 'w')
+# for i in wordcount:
+#     output = i + ':' + str(wordcount[i])
+#     print(output)
+#     filename_output.write(output+'\n')
+# filename_output.close()
+
+f_output = open("output1.txt", "w")
+f_output.write(str(wordcount))
+f_output.close
 
