e68506e5936f1637b6161868da22448779722ff5
lavanyagadde <lavanyagadde217@gmail.com>
2020-06-11 14:38:54 -0500

    comments

--- ICP2/sourcecode/weigntsconversion.py
+++ ICP2/sourcecode/weigntsconversion.py
@@ -1,9 +1,9 @@
 # converting weights into kgs
 N = int(input("Enter number of students? "))
-lbs = []
+lbs = []    # defining the list
 for i in range(N):
     lb = float(input("Enter the weight of each student: "))
-    lbs.append(lb)
+    lbs.append(lb)      # appending the values to  a list
 print("Given weights in lbs ", lbs)
 
 # using list comprehension to convert lbs to kgs
